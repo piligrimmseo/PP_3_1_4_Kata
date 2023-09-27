f4bbd9677c09a71fe69a03b45ede0ef299be7a34
Dmitrii <piligrimm.seo@gmail.com>
2023-09-27 21:53:23 +0300

    Initial commit

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -0,0 +1,57 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.models.Role;
+import ru.kata.spring.boot_security.demo.models.User;
+import ru.kata.spring.boot_security.demo.repositories.RoleRepository;
+import ru.kata.spring.boot_security.demo.services.UserService;
+
+import java.security.Principal;
+import java.util.List;
+
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+    private UserService userService;
+    private RoleRepository roleRepository;
+
+    @Autowired
+    public AdminController(UserService userService, RoleRepository roleRepository) {
+        this.userService = userService;
+        this.roleRepository = roleRepository;
+    }
+
+    @GetMapping()
+    public String bootstrap(@ModelAttribute("user") User user, Principal principal, Model model) {
+        User users = userService.findByUsername(principal.getName());
+        model.addAttribute("user", users);
+        model.addAttribute("users", userService.getAllUsers());
+        List<Role> roles = (List<Role>) roleRepository.findAll();
+        model.addAttribute("allRoles", roles);
+        return "boot";
+    }
+
+
+    @PatchMapping("{id}/edit")
+    public String edit(@ModelAttribute("user") User user, Model model, @PathVariable("id") int id) {
+        userService.updateUserById(id, user);
+        return "redirect:/admin/";
+    }
+
+    @DeleteMapping("/admin/{id}")
+    public String delete(@PathVariable("id") int id) {
+        userService.deleteUserById(id);
+        return "redirect:/admin/";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("user") User user) {
+        userService.saveUser(user);
+        return "redirect:/admin/";
+    }
+
+}
